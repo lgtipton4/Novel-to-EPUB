24318f2c65364ccc5d9e63934f41fb421b70042b
lgtipton4 <lgtipton4@gmail.com>
2025-03-11 19:33:15 -0400

    Added Send to Phone, threading to fix GUI responsiveness

--- novel2epub.py
+++ novel2epub.py
@@ -1,13 +1,8 @@
-# OVERALL TODO
-# Implement automated updates
-# Implement tray with pstray
-# Implement history section in window and "resend" button.
-# Add automated sending to iPhone via vtext. User can setup with phone number 
-
 import validators
 import os
 import royalroad
 import email_function
+import threading
 
 import tkinter as tk
 from tkinter import ttk
@@ -22,8 +17,8 @@ class Novel2EPUB:
         self.root.title("Novel2EPUB")
         self.root.resizable(False, False)
 
-        window_width = 346
-        window_height = 146
+        window_width = 400
+        window_height = 200
 
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
@@ -37,61 +32,111 @@ class Novel2EPUB:
         self.entry = ttk.Entry(self.root, width=50)
         self.entry.pack(padx=20, pady=10)
         self.root.iconbitmap('./assets/icon.ico')
-        self.checkbox = tk.BooleanVar(value=False) 
+        self.kindle_checkbox = tk.BooleanVar(value=False) 
+        self.phone_checkbox = tk.BooleanVar(value=False)
 
         ttk.Checkbutton(root,  
                 text="Send to Kindle", 
-                variable=self.checkbox,
+                variable=self.kindle_checkbox,
+                onvalue=True,
+                offvalue=False).pack()
+        
+        ttk.Checkbutton(root,
+                text="Send to Phone",
+                variable=self.phone_checkbox,
                 onvalue=True,
                 offvalue=False).pack()
 
-        self.button = ttk.Button(self.root, text="Submit", command=self.on_click)
+        self.button = ttk.Button(self.root, text="Download", command=self.on_click)
         self.button.pack(padx=20, pady=10)
+
+        self.progress_bar = ttk.Progressbar(root,
+                                orient = "horizontal",
+                                mode="indeterminate",
+                                length=280)
 
         self.book_title = None
 
-        self.root.geometry(f"346x146+{center_x}+{center_y}")
+        self.root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
 
-    def box_ticked(self):
+    def create_thread(self, target_function, arguments):
+        thread = threading.Thread(target=target_function, args=arguments)
+        thread.start()
+    
+    def kindle_box_ticked(self):
         if not os.path.exists("config.txt"):
             showinfo(title="Error", message="Config file not found. Please run setup batch file.")
             return
         
         with open("config.txt", "r") as f:
             kindle_email = f.readline()
+            phone_address = f.readline()
             if(kindle_email == "N/A"):
                 showinfo(title="Error", message="Kindle email not found in config file. Please run the setup batch file again.")
                 os.remove("config.txt")
                 return
             else:
-                print(kindle_email)
                 email_function.send_email(kindle_email, f'{self.book_title}.epub')
-
-    def on_click(self):
-        user_input = self.entry.get()
+                self.on_success()
+    
+    def phone_box_ticked(self):
+        if not os.path.exists("config.txt"):
+            showinfo(title="Error", message="Config file not found. Please run setup batch file.")
+            return
+        
+        with open("config.txt", "r") as f:
+            kindle_email = f.readline()
+            phone_address = f.readline()
+        
+        if(phone_address == "N/A"):
+                showinfo(title="Error", message="Phone number not found in config file. Please run the setup batch file again.")
+                os.remove("config.txt")
+                return
+        else:
+            email_function.send_email(phone_address, f'{self.book_title}.epub')
+            self.on_success()
+    
+    def start_download(self, user_input):
         if validators.url(user_input):
+            self.progress_bar.pack()
+            self.progress_bar.start()
             book_title = royalroad.fetch_title(royalroad.make_request(user_input))
             self.book_title = book_title
-            royalroad.pack_epub(user_input)
+            self.checkbox_check()
 
         else:
             book_title, book_url = royalroad.search(royalroad.make_request(f'{main_url}/fictions/search?globalFilters=false&title={user_input}&orderBy=popularity'))
             self.book_title = book_title
-             #print(self.book_title)
             answer = askyesno(title='Confirmation',
-                            message=f'Is {book_title} ({book_url} the novel you are looking for?)'
+                            message=f'Is {book_title} ({book_url}) the novel you are looking for?'
                             )
 
             if(answer):
+                self.progress_bar.pack() 
+                self.progress_bar.start()
                 royalroad.pack_epub(book_url)
+                self.checkbox_check()
 
             else:
                 showinfo(title="Notice", message="Try re-entering the title in a more complete manner. (ex. mother -> mother of learning) or entering the url directly.")
+                return 
+            
+    def on_click(self):
+        user_input = self.entry.get()
+        self.create_thread(self.start_download, (user_input,)) 
+    
+    def checkbox_check(self):
+        if self.kindle_checkbox.get(): 
+            self.create_thread(self.kindle_box_ticked, ())
 
-        if self.checkbox.get(): 
-            self.box_ticked()
-        
-        showinfo(title="Success!", message=f"{book_title} successfully downloaded.")
+        if self.phone_checkbox.get():
+            self.create_thread(self.phone_box_ticked, ())
+
+    def on_success(self):
+        self.progress_bar.stop()
+        self.progress_bar.pack_forget()
+        showinfo(title="Success!", message=f"{self.book_title} successfully downloaded.")
+        return
 
 def main():
     root = tk.Tk()
--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo off
-if exist "config.txt" exit
+if exist "config.txt" goto exists
 :top
 cls
 echo SETUP
@@ -12,6 +12,17 @@ if "%c%"=="y" goto kindle_setup
 echo N/A >> config.txt
 goto next
 
+:next
+cls
+echo --------------------------------------------------------
+echo Would you like to pair a phone with this software? (Y/N) 
+echo --------------------------------------------------------
+set /p c=
+if "%c%"=="Y" goto phone_setup
+if "%c%"=="y" goto phone_setup
+echo N/A >> config.txt
+exit
+
 :kindle_setup
 cls
 echo Please enter the email associated with your Kindle. If you are unsure of where to find it, review the documentation (https://github.com/lgtipton4/Novel-to-EPUB)
@@ -19,17 +30,22 @@ set /p kindle_email=
 echo %kindle_email% >> config.txt
 goto next
 
-:next
+:phone_setup
 cls
-echo --------------------------------------------------------
-echo Would you like to opt in to automatic chapter downloads? 
-echo --------------------------------------------------------
-set /p c=
-if "%c%"=="Y" goto automatic_chapter_updates
-if "%c%"=="y" goto automatic_chapter_updates
-echo N/A >> config.txt
+echo Please enter your phone number with NO spaces. ex. 1234567890
+set /p phone_number=
+cls
+echo Please go to this website (https://www.textsendr.com/emailsms.php), locate your carrier and paste everything after and including the "@" symbol. ex. @tmomail.com. Note that Verizon Wireless no longer supports this feature.
+timeout /t 3 >nul
+start https://www.textsendr.com/emailsms.php
+set /p suffix=
+echo %phone_number%%suffix% >> config.txt
+cls
+echo Succesfully setup.
+pause
 exit
 
-:automatic_chapter_updates
-echo automatic_chapter_updates >> config.txt
+:exists
+echo Setup has already been run. If you wish to change settings, delete the "config.txt" file and rerun this file.
+pause
 exit
